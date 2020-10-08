5b409a4d9f75b345073426f61aa27783a664e392
dsilvaba <49752895+dsilvaba@users.noreply.github.com>
2020-10-08 10:58:52 +0200

    login

--- NinjaManagerASP/NinjaManagerASP/DataModel.Context.cs
+++ NinjaManagerASP/NinjaManagerASP/DataModel.Context.cs
@@ -25,14 +25,8 @@ namespace NinjaManagerASP
             throw new UnintentionalCodeFirstException();
         }
     
-        public virtual DbSet<C__RefactorLog> C__RefactorLog { get; set; }
         public virtual DbSet<Category> Categories { get; set; }
         public virtual DbSet<Equipment> Equipments { get; set; }
         public virtual DbSet<Ninja> Ninjas { get; set; }
-        public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
-        public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
-        public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
-        public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
-        public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
     }
 }
