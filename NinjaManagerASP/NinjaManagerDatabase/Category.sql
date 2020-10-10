c7162fd9348bd4074ec17128145c0dfb53557980
Dolflot <49400268+Dolflot@users.noreply.github.com>
2020-10-10 15:57:38 +0200

    gitignore test

--- NinjaManagerASP/NinjaManagerDatabase/Category.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[Category]
-(
-    [CategoryName] NCHAR(50) NOT NULL, 
-    PRIMARY KEY ([CategoryName]) 
-)
--- NinjaManagerASP/NinjaManagerDatabase/Ninja.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Ninja]
-(
-	[Id] INT NOT NULL  IDENTITY, 
-    [Name] NCHAR(50) NOT NULL, 
-    [Gold] INT NOT NULL, 
-    CONSTRAINT [PK_Ninja] PRIMARY KEY ([Id]) 
-)
